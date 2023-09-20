389a6e990e12bbf055172edcb688bc0af975fdc7
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-20 08:03:48 +0100

    cleanup

--- http/handle_index.go
+++ http/handle_index.go
@@ -6,41 +6,6 @@ import (
 	"github.com/adoublef/clear-carrot/templates"
 )
 
-// const (
-// 	layoutsDir   = "templates/layouts"
-// 	templatesDir = "templates"
-// 	extension    = "/*.html"
-// )
-
-// var (
-// 	//go:embed templates/*
-// 	files     embed.FS
-// 	templates map[string]*template.Template
-// )
-
-// func init() {
-// 	if templates == nil {
-// 		templates = make(map[string]*template.Template)
-// 	}
-// 	tmplFiles, err := fs.ReadDir(files, templatesDir)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	for _, tmpl := range tmplFiles {
-// 		if tmpl.IsDir() {
-// 			continue
-// 		}
-
-// 		pt, err := template.ParseFS(files, templatesDir+"/"+tmpl.Name())
-// 		if err != nil {
-// 			panic(err)
-// 		}
-
-// 		templates[tmpl.Name()] = pt
-// 	}
-// }
-
 func (s *Service) handleIndex() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
--- templates/templates.go
+++ templates/templates.go
@@ -15,3 +15,38 @@ var IndexPage *template.Template
 func init() {
 	IndexPage = template.Must(template.ParseFS(indexFS, "index.html"))
 }
+
+// const (
+// 	layoutsDir   = "templates/layouts"
+// 	templatesDir = "templates"
+// 	extension    = "/*.html"
+// )
+
+// var (
+// 	//go:embed templates/*
+// 	files     embed.FS
+// 	templates map[string]*template.Template
+// )
+
+// func init() {
+// 	if templates == nil {
+// 		templates = make(map[string]*template.Template)
+// 	}
+// 	tmplFiles, err := fs.ReadDir(files, templatesDir)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	for _, tmpl := range tmplFiles {
+// 		if tmpl.IsDir() {
+// 			continue
+// 		}
+
+// 		pt, err := template.ParseFS(files, templatesDir+"/"+tmpl.Name())
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		templates[tmpl.Name()] = pt
+// 	}
+// }
