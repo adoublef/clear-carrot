43bdaa2dddf0a8f276cf124109a5bd31b8764784
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 23:46:02 +0100

    using teh jsx-like components

--- internal/iam/http/hande_index.go
+++ internal/iam/http/hande_index.go
@@ -1,78 +1,34 @@
 package http
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
+	"github.com/adoublef/clear-carrot/jsx/dom"
 	g "github.com/maragudk/gomponents"
-	hx "github.com/maragudk/gomponents-htmx"
-	c "github.com/maragudk/gomponents/components"
 	. "github.com/maragudk/gomponents/html"
 	// hx "github.com/maragudk/gomponents-htmx"
 )
 
 func (s *Service) handleIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := Page("Hi!", r.URL.Path).Render(w)
+		page := &dom.Html{
+			Title:   "Golang 💛 Htmx",
+			BaseURL: r.URL,
+		}
+
+		err := page.Render(w,
+			Header(Nav(
+				A(Href("/"), g.Text("home")),
+				Ul(
+					Li(A(Href("/signin"), g.Text(("signin")))),
+					Li(A(Href("/signout"), g.Text(("signout")))),
+					// Li(A(Href("/settings"),g.Text(("settings")))),
+				),
+			)),
+			Main())
 		if err != nil {
 			http.Error(w, "error parsing template: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
-}
-
-func Page(title, currentPath string) g.Node {
-	return Doctype(
-		HTML(
-			Lang("en"),
-			Head(
-				Meta(Charset("utf-8")),
-				TitleEl(g.Text(title)),
-				Link(Href("/static/index.css"), Rel("stylesheet")),
-				Script(Src("/static/htmx.min.js"), Defer()),
-				Script(Src("/static/hyperscript.min.js"), Defer()),
-			),
-			Body(hx.Boost("true"),
-				Navbar(currentPath),
-				H1(g.Text(title)),
-				P(g.Textf("Welcome to the page at %v.", currentPath)),
-				Counter(0),
-			),
-		),
-	)
-}
-
-func Navbar(currentPath string) g.Node {
-	anchor := func(href, name, currentPath string) g.Node {
-		return A(Href(href), c.Classes{"is-active": currentPath == href}, g.Text(name))
-	}
-
-	return Nav( anchor("/", "Home", currentPath), anchor("/about", "About", currentPath))
-}
-
-func (s *Service) handleCount() http.HandlerFunc {
-	parseInt := func(r *http.Request) int {
-		count := r.URL.Query().Get("count")
-		if count == "" {
-			count = "0"
-		}
-		n, _ := strconv.Atoi(count)
-
-		return n
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := Counter(parseInt(r)).Render(w)
-		if err != nil {
-			http.Error(w, "error parsing counter template: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-}
-
-func Counter(count int) g.Node {
-	url := fmt.Sprintf("/?count=%d", count+1)
-	return Span(g.Textf("%d", count),
-		Button(g.Text("click"), hx.Post(url), hx.Target("closest span"), hx.Swap("outerHTML")))
 }
