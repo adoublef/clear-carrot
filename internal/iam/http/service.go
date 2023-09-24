54a76ecd72fd43e0c09506b335ef066a97449209
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 23:07:16 +0100

    add back the internal package

---
+++ internal/iam/http/service.go
@@ -0,0 +1,31 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+var _ http.Handler = (*Service)(nil)
+
+type Service struct {
+	m *chi.Mux
+}
+
+// ServeHTTP implements http.Handler.
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.m.ServeHTTP(w, r)
+}
+
+func NewService() *Service {
+	s := Service{
+		m: chi.NewMux(),
+	}
+	s.routes()
+	return &s
+}
+
+func (s *Service) routes() {
+	s.m.Get("/*", s.handleIndex())
+	s.m.Post("/", s.handleCount())
+}
